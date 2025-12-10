700a83fb3ae5c18cb417ea462ebe11be02fe7f97
M1raclEEEEE <lh20050323@gmail.com>
2025-12-10 15:50:32 +0800

    Create download_model.py

---
+++ model/download_model.py
@@ -0,0 +1,18 @@
+# scripts/download_model.py
+import os
+
+print("=" * 50)
+print("模型下载指引")
+print("=" * 50)
+print("由于模型文件较大，请手动下载：")
+print("")
+print("方法1：从百度网盘下载")
+print("链接: https://pan.baidu.com/s/12idZgzXu28ngGCYrSj-Pwpwd=1234")
+print("提取码: 1234")
+print("下载后保存为: model/best_model.pth")
+print("")
+print("方法2：运行以下命令重命名现有模型")
+print("mv model/best_model.pth models/best_model.pth")
+print("mkdir -p models")
+print("")
+print("=" * 50)
